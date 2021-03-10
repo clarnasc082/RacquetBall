4d2985d6cc774b56d3a331d25c89ef0cb7be4edd
Clara Nascu <clarnasc082@ed.amdsb.ca>
2021-03-10 11:59:59 -0500

    Add project files.

---
+++ RacquetBall/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace RacquetBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.P2ScoreLabel = new System.Windows.Forms.Label();
+            this.P1ScoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // P2ScoreLabel
+            // 
+            this.P2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.P2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P2ScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.P2ScoreLabel.Location = new System.Drawing.Point(398, 43);
+            this.P2ScoreLabel.Name = "P2ScoreLabel";
+            this.P2ScoreLabel.Size = new System.Drawing.Size(80, 46);
+            this.P2ScoreLabel.TabIndex = 3;
+            this.P2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // P1ScoreLabel
+            // 
+            this.P1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.P1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P1ScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.P1ScoreLabel.Location = new System.Drawing.Point(284, 43);
+            this.P1ScoreLabel.Name = "P1ScoreLabel";
+            this.P1ScoreLabel.Size = new System.Drawing.Size(78, 46);
+            this.P1ScoreLabel.TabIndex = 4;
+            this.P1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.P1ScoreLabel);
+            this.Controls.Add(this.P2ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label P2ScoreLabel;
+        private System.Windows.Forms.Label P1ScoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
